1051e3ccd595c3939a1da1d3972e8d873bdfaee0
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-10-21 09:22:05 -0500

    math_python
    math_python

---
+++ math/main.py
@@ -0,0 +1,5 @@
+import math  # Importa el módulo 'math', que contiene funciones y constantes matemáticas.
+
+print(math.pi)   # Imprime el valor de π (pi), una constante matemática ≈ 3.141592653589793.
+print(math.e)    # Imprime el valor de e, la base de los logaritmos naturales ≈ 2.718281828459045.
+print(math.tau)  # Imprime el valor de τ (tau), que es el doble de pi (2 * π) ≈ 6.283185307179586.
